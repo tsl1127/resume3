4682f03359afe1f85bd31209a75ccb7bda880d99
tsl <taosiliang1127tsl@163.com>
2018-02-18 21:59:31 +0800

    MVC的C思想更新下,bindEvents

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,15 +1,26 @@
 !function(){
     var view = document.querySelector('#topNavBar')
     // view.style.border = '1px solid red'
-    var controller = function(view){
-        window.addEventListener('scroll',function(x){    //加事件监听队列
-            if(window.scrollY >0){
-                view.classList.add('sticky')
-            }else{
-                view.classList.remove('sticky')
-            }
-        })          
+    var controller = {
+        view:null,
+        init: function(view){
+            console.log('1')
+            console.log(this)
+            this.view = view
+            this.bindEvents()    //等价于this.bindEvents.call(this)                 
+            },
+        bindEvents:function(){
+            console.log('2')
+            console.log(this)
+            var view = this.view
+            window.addEventListener('scroll',function(x){    //加事件监听队列
+                if(window.scrollY >0){
+                    view.classList.add('sticky')
+                }else{
+                    view.classList.remove('sticky')
+                }
+            })
         }
-        // controller.call(null,view)
-        controller(view)
+    }
+    controller.init(view)  //等价于controller.init.call(controller,view)
 }.call()
